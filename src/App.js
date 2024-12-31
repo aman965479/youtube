04b35116e560e6c3a6a7de381805e253d5b2dae3
aman965479 <aman965479@gmail.com>
2024-12-31 19:35:01 +0530

    changed App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Aman CH
+          Learn React
         </a>
       </header>
     </div>
